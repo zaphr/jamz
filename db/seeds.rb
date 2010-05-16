223d523bbf74bd387706e9031a8e2bce34140457
Anthony Reinbach <areinbach@gmail.com>
2010-05-16 19:37:53 +1000

    Wayy too much and too long bwn checkins.. jQuery mainly

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,4 +35,29 @@ tab = Tab.new(:capo_position => '0')
 tab.save
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 1, :row_no => 1)  
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 2, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 3, :row_no => 1)  
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 4, :row_no => 1)
+Song.create(:name => 'Jane Says', :artist => "Jane's Addiction", :youtube_id => "xh-5FI21s6M", :tab => tab)
+
+
+tab = Tab.new(:capo_position => '2')
+tab.save
+
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 1, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'C').first, :column_no => 2, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Dm').first, :column_no => 3, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'F').first, :column_no => 4, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 1, :row_no => 2)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'C').first, :column_no => 2, :row_no => 2)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'E').first, :column_no => 3, :row_no => 2)
+
+
+Song.create(:name => 'House of the Rising Sun', :artist => 'Tommy Emmanuel', :youtube_id => "WIWSVTaytOw", :tab => tab)
+
+tab = Tab.new(:capo_position => '0')
+tab.save
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 1, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 2, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 3, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 4, :row_no => 1)
 Song.create(:name => 'Jane Says', :artist => "Jane's Addiction", :youtube_id => "xh-5FI21s6M", :tab => tab)
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,106 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function(){
+
+//Larger thumbnail preview 
+
+$("ul.thumb li").hover(function() {
+	$(this).css({'z-index' : '10'});
+	$(this).find('img').addClass("hover").stop()
+		.animate({
+			marginTop: '-110px', 
+			marginLeft: '-110px', 
+			top: '50%', 
+			left: '50%', 
+			width: '174px', 
+			height: '174px',			
+			padding: '20px' 
+
+		}, 200);
+
+	} , function() {
+	$(this).css({'z-index' : '0'});
+	$(this).find('img').removeClass("hover").stop()
+		.animate({
+			marginTop: '0', 
+			marginLeft: '0',
+			top: '0', 
+			left: '0', 
+			width: '100px', 
+			height: '100px', 
+			padding: '5px'
+		}, 400);
+});
+
+//Swap Image on Click
+	$("ul.thumb li a").click(function() {
+
+		var mainImage = $(this).attr("href"); //Find Image Name
+		$("#main_view img").attr({ src: mainImage });
+		return false;		
+	});
+
+});
+
+
+    $(function(){
+	  $("button").click(function(){
+		alert("Watcha looking for Willis??");
+		return false;
+	});
+});
+
+    $(function(){
+	  $("#fade-out-tab").click(function(){
+      $("#fade-out-tab").hide();
+	  $('.thumb').fadeOut();
+      $("#fade-in-tab").show();
+		return false;
+	});
+});
+
+
+
+    $(function(){
+	  $("#fade-in-tab").click(function(){
+      $("#fade-in-tab").hide();
+	  $('.thumb').fadeIn();
+      $("#fade-out-tab").show();          
+		return false;
+	});
+});
+
+$(function(){
+      $("#fade-out-tab").hide();
+	  $('.thumb').hide();
+});
+
+$(function(){
+    $("button").button();
+    populateElement('#SearchText', 'Find a song');
+});
+
+
+
+
+
+
+ function populateElement(selector, defvalue) {
+    $(selector).each(function() {
+        if($.trim(this.value) == "") {
+            this.value = defvalue;
+        }
+    });
+
+    $(selector).focus(function() {
+        if(this.value == defvalue) {
+            this.value = "";
+        }
+    });
+
+    $(selector).blur(function() {
+        if($.trim(this.value) == "") {
+            this.value = defvalue;
+        }
+    });
+ }
+
+
