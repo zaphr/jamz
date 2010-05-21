8c70cec2c1e7f7d79b649128d389c3b4f44a75a9
Anthony Reinbach <areinbach@gmail.com>
2010-05-21 22:42:36 +1000

    got ugly chord name layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,15 @@ TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'F').first, :colu
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 1, :row_no => 2)
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'C').first, :column_no => 2, :row_no => 2)
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'E').first, :column_no => 3, :row_no => 2)
-
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 1, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'C').first, :column_no => 2, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Dm').first, :column_no => 3, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'F').first, :column_no => 4, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Dm').first, :column_no => 5, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'F').first, :column_no => 6, :row_no => 3)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 1, :row_no => 4)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'E').first, :column_no => 2, :row_no => 4)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'Am').first, :column_no => 3, :row_no => 4)
 
 Song.create(:name => 'House of the Rising Sun', :artist => 'Tommy Emmanuel', :youtube_id => "WIWSVTaytOw", :tab => tab)
 
@@ -58,6 +66,6 @@ tab = Tab.new(:capo_position => '0')
 tab.save
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 1, :row_no => 1)
 TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 2, :row_no => 1)
-TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 3, :row_no => 1)
-TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 4, :row_no => 1)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'G').first, :column_no => 3, :row_no => 2)
+TabPosition.create(:tab => tab, :chord => Chord.where(:name => 'A').first, :column_no => 4, :row_no => 2)
 Song.create(:name => 'Jane Says', :artist => "Jane's Addiction", :youtube_id => "xh-5FI21s6M", :tab => tab)
